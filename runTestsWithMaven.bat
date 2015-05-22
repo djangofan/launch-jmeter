851727eaa5b41ff64a02914345ea66d64e52b0b1
Jon Austen <austenjt@gmail.com>
2015-05-21 22:19:48 -0700

    Minor edit.

---
+++ runTestsWithMaven.bat
@@ -0,0 +1,14 @@
+@ECHO off
+SET WINDOW_TITLE=Jmeter via Maven launcher
+TITLE=%WINDOW_TITLE%
+ECHO Closing this window will close Jmeter...
+IF NOT DEFINED M2_HOME ECHO It appears you are missing the Maven installation on your system.
+IF NOT DEFINED JAVA_HOME ECHO You need to define JAVA_HOME on your system.
+
+CALL mvn verify -Prun-performance-tests
+
+::-------------------------------------------------------------------
+::  End script
+::-------------------------------------------------------------------
+:END
+FOR /l %%a IN (10,-1,1) DO (TITLE %WINDOW_TITLE% -- Closing in %%as&PING.exe -n 2 -w 1 127.0.0.1>nul)
